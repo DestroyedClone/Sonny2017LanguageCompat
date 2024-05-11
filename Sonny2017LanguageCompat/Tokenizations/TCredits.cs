9c2dccdf4633699b21b4eb8326ab918cbdbcc15f
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-05-11 02:00:08 -0700

    Replaced token swap with TokenTranslator component

--- Sonny2017LanguageCompat/Tokenizations/TCredits.cs
+++ Sonny2017LanguageCompat/Tokenizations/TCredits.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static Mono.Security.X509.X520;
 
 namespace Sonny2017LanguageCompat.Tokenizations
 {
@@ -10,6 +11,24 @@ namespace Sonny2017LanguageCompat.Tokenizations
         {
             On.CreditScene.AddCreditName += CreditScene_AddCreditName;
             On.CreditScene.CreateCreditRole += CreditScene_CreateCreditRole;
+            On.CreditName.Deploy += CreditName_Deploy;
+            On.Credit.Deploy += Credit_Deploy;
+        }
+
+        private static void Credit_Deploy(On.Credit.orig_Deploy orig, Credit self)
+        {
+            orig(self);
+            var comp = self.header.gameObject.AddComponent<TokenTranslator>();
+            comp.textController = self.header;
+            comp.token = self.name;
+        }
+
+        private static void CreditName_Deploy(On.CreditName.orig_Deploy orig, CreditName self, string name, UnityEngine.Vector2 position, UnityEngine.GameObject parent)
+        {
+            orig(self, name, position, parent);
+            var comp = self.text.gameObject.AddComponent<TokenTranslator>();
+            comp.textController = self.text;
+            comp.token = name;
         }
 
         private static Credit CreditScene_CreateCreditRole(On.CreditScene.orig_CreateCreditRole orig, CreditScene self, string roleName)
@@ -17,58 +36,75 @@ namespace Sonny2017LanguageCompat.Tokenizations
             switch (roleName)
             {
                 case "Sonny":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_SONNY");
+                    roleName = "CREDIT_ROLE_SONNY";
                     break;
+
                 case "by ArmorGames Studio":
-                    roleName = LanguageManager.GetString("CREDIT_ARMORGAMES");
+                    roleName = "CREDIT_ARMORGAMES";
                     break;
+
                 case "Executive Producer":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_EXECUTIVEPRODUCER");
+                    roleName = "CREDIT_ROLE_EXECUTIVEPRODUCER";
                     break;
+
                 case "Producer and Lead Developer":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_PRODUCERANDLEADDEVELOPER");
+                    roleName = "CREDIT_ROLE_PRODUCERANDLEADDEVELOPER";
                     break;
+
                 case "Engineering":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_ENGINEERING");
+                    roleName = "CREDIT_ROLE_ENGINEERING";
                     break;
+
                 case "Lead Artist":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_LEADARTIST");
+                    roleName = "CREDIT_ROLE_LEADARTIST";
                     break;
+
                 case "Environmental Design":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_ENVIRONMENTALDESIGN");
+                    roleName = "CREDIT_ROLE_ENVIRONMENTALDESIGN";
                     break;
+
                 case "Environmental Texture":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_ENVIRONMENTALTEXTURE");
+                    roleName = "CREDIT_ROLE_ENVIRONMENTALTEXTURE";
                     break;
+
                 case "UI Art and Design":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_UIARTANDDESIGN");
+                    roleName = "CREDIT_ROLE_UIARTANDDESIGN";
                     break;
+
                 case "Sound and Music":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_SOUNDANDMUSIC");
+                    roleName = "CREDIT_ROLE_SOUNDANDMUSIC";
                     break;
+
                 case "Story":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_STORY");
+                    roleName = "CREDIT_ROLE_STORY";
                     break;
+
                 case "Voice Acting":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_VOICEACTING");
+                    roleName = "CREDIT_ROLE_VOICEACTING";
                     break;
+
                 case "Original Song":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_ORIGINALSONG");
+                    roleName = "CREDIT_ROLE_ORIGINALSONG";
                     break;
+
                 case "Project Manager":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_PROJECTMANAGER");
+                    roleName = "CREDIT_ROLE_PROJECTMANAGER";
                     break;
+
                 case "Production Support":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_PRODUCTIONSUPPORT");
+                    roleName = "CREDIT_ROLE_PRODUCTIONSUPPORT";
                     break;
+
                 case "DDF Test Team":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_DDFTESTTEAM");
+                    roleName = "CREDIT_ROLE_DDFTESTTEAM";
                     break;
+
                 case "QA & Test Engineering":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_QAANDTESTENGINEERING");
+                    roleName = "CREDIT_ROLE_QAANDTESTENGINEERING";
                     break;
+
                 case "Gameplay Testing":
-                    roleName = LanguageManager.GetString("CREDIT_ROLE_GAMEPLAYTESTING");
+                    roleName = "CREDIT_ROLE_GAMEPLAYTESTING";
                     break;
             }
             return orig(self, roleName);
@@ -77,7 +113,7 @@ namespace Sonny2017LanguageCompat.Tokenizations
         private static void CreditScene_AddCreditName(On.CreditScene.orig_AddCreditName orig, CreditScene self, string name, Credit credit)
         {
             if (name == "by ArmorGames Studio")
-                name = LanguageManager.GetString("CREDIT_ARMORGAMES");
+                name = "CREDIT_ARMORGAMES";
             orig(self, name, credit);
         }
     }
