18da5204125e50da1a28c0764fd870047e2ba511
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-05-12 08:37:24 -0700

    Adjusted Common and adding version tokens to language files

--- Sonny2017LanguageCompat/Common.cs
+++ Sonny2017LanguageCompat/Common.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace Sonny2017LanguageCompat
 {
     internal static class Common
     {
+        public const string VersionToken = "COMMON_VERSION";
         public static string Version
         {
             get
             {
-                return "Version 1.6.7";
+                return LanguageManager.GetString("COMMON_VERSION", VersionNumber);
             }
+        }
+
+        public static string VersionNumber = "1.6.7";
+
+        public static bool TryGetComponent<TComponent>(this GameObject gameObject, out TComponent component)
+        {
+            component = gameObject.GetComponent<TComponent>();
+            return component != null;
         }
     }
 }
