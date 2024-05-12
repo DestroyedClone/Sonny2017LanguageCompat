6293f085c501e8f018ec0e332ce1e2d3c570871e
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-05-12 07:59:59 -0700

    Added common class for repeated referenced strings

---
+++ Sonny2017LanguageCompat/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sonny2017LanguageCompat
+{
+    internal static class Common
+    {
+        public static string Version
+        {
+            get
+            {
+                return "Version 1.6.7";
+            }
+        }
+    }
+}
